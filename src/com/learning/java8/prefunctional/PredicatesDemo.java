2c2a8d0ddf96af87debeabb37b3886c881d66a90
Abhishek <sharma.abhishek1739@gmail.com>
2022-08-25 21:10:03 +0530

    Added necessary comments on PredicatesDemo and SupplierDemo classes.

--- src/com/learning/java8/prefunctional/PredicatesDemo.java
+++ src/com/learning/java8/prefunctional/PredicatesDemo.java
@@ -2,6 +2,14 @@ package com.learning.java8.prefunctional;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
+
+/*
+    Predicate is a Functional Interface, introduced in Java 8.
+    Predicate FI contains abstract method which accepts the input of any Type (Generic Type)
+    and returns boolean (Returns true if the input matches the predicate or else it return false).
+    For reference : Check Predicate Interface
+ */
 
 public class PredicatesDemo {
 
@@ -9,6 +17,10 @@ public class PredicatesDemo {
 
         List<Integer> list= Arrays.asList(1,2,3,4,5);
 
+        /*
+            filter() method from stream API uses Predicate Object as a input Parameter which can be
+            written as lambda expression as below expression
+         */
         list.stream().filter(t -> t%2 == 0).forEach(t->System.out.println("Printing Even: "+t));
 
 
--- src/com/learning/java8/prefunctional/SupplierDemo.java
+++ src/com/learning/java8/prefunctional/SupplierDemo.java
@@ -5,12 +5,25 @@ import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Supplier;
 
+/*
+    Supplier is a Functional Interface, introduced in Java 8.
+    Supplier FI contains abstract method with no parameters and returns dummy data to fulfill
+     the need of Logic.
+    For reference : Check Supplier Interface
+ */
+
 public class SupplierDemo {
 
 
     public static void main(String[] args){
         List<Integer> list= Arrays.asList();//(1,2,3,4,5);
 
+        /*
+            findAny() method returns value if it collection is having.
+            if collection is empty then it will return the value passed by Supplier as shown below
+            orElseGet() method from stream API uses Supplier Object as a input Parameter which can be
+            written as lambda expression as below expression
+         */
         System.out.println(list.stream().findAny().orElseGet(()-> 20));
     }
 }
